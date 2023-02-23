b206a997a9025253e9ceffcb80069e294bd4037b
makardom <70172017+makardom@users.noreply.github.com>
2023-02-23 16:45:19 +0300

    Add files via upload

---
+++ chapter_1/lab1_5/Main.java
@@ -0,0 +1,9 @@
+package lab1.zad5;
+public class Main {
+    public static void main(String[] args) {
+        int number = (int) Double.MAX_VALUE;
+        System.out.println("Result:" +
+                "\nint:    " + number +
+                "\ndouble: " + Double.MAX_VALUE);
+    }
+}
